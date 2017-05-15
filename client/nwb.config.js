2b7caa0b00e4031484265cc15ba215fb06f5c883
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-05-15 18:02:18 -0300

    Create Initial Client Structure

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  type: 'react-app'
+  type: 'react-app',
+  webpack: {
+    extra: {
+      // Example of adding an extra loader which isn't managed by nwb,
+      // assuming you've installed html-loader in your project.
+      resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.css'],
+        alias: {
+          modules: __dirname + '/node_modules'
+        }
+      },
+      module: {
+        rules: [{
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react', 'stage-2']
+          }
+        }]
+      },
+      // Example of adding an extra plugin which isn't managed by nwb
+      plugins: [
+
+      ]
+    }
+  }
 }
