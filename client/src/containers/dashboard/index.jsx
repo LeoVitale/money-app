50a30e92c3bc16da8e7ba8bdbf799472267660e1
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-05-19 01:07:05 -0300

    Create Value Box and some Widgets

--- client/src/containers/dashboard/index.jsx
+++ client/src/containers/dashboard/index.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import ContentHeader from '../../common/template/content-header';
 import Content from '../../common/template/content';
+import ValueBox from '../../common/widget/value-box';
+import Row from '../../common/layout/row';
 
 class DashBoard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title='Dashnoard' small='Versão 1.0' />
-        <Content>
-          <h1>opa</h1>
-        </Content>
+        <Row>
+          <Content>
+            <ValueBox cols="12 4" color="green" icon="bank" value="R$ 10" text="Total de Créditos" />
+            <ValueBox cols="12 4" color="red" icon="credit-card" value="R$ 10" text="Total de Débitos" />
+            <ValueBox cols="12 4" color="blue" icon="money" value="R$ 10" text="Valor Consolidado" />
+          </Content>
+        </Row>
       </div>
     );
   }
