2ebb6188f53ca09d4a6b51cf6d7bd0de10a23113
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-05-19 02:11:51 -0300

    Init Config Redux

--- client/src/containers/dashboard/index.jsx
+++ client/src/containers/dashboard/index.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import {getSummary} from './dashboardActions';
 import ContentHeader from '../../common/template/content-header';
 import Content from '../../common/template/content';
 import ValueBox from '../../common/widget/value-box';
 import Row from '../../common/layout/row';
 
 class DashBoard extends Component {
+
+  componentWillMount() {
+    this.props.getSummary();
+  }
+
   render() {
+    const { credit, debt } = this.props.summary;
     return (
       <div>
-        <ContentHeader title='Dashnoard' small='Versão 1.0' />
+        <ContentHeader title='Dashnoard' small='Versão 1.0'/>
         <Row>
           <Content>
-            <ValueBox cols="12 4" color="green" icon="bank" value="R$ 10" text="Total de Créditos" />
-            <ValueBox cols="12 4" color="red" icon="credit-card" value="R$ 10" text="Total de Débitos" />
-            <ValueBox cols="12 4" color="blue" icon="money" value="R$ 10" text="Valor Consolidado" />
+            <ValueBox cols="12 4" color="green" icon="bank" value={`R$ ${credit}`} text="Total de Créditos" />
+            <ValueBox cols="12 4" color="red" icon="credit-card" value={`R$ ${debt}`} text="Total de Débitos" />
+            <ValueBox cols="12 4" color="blue" icon="money" value={`R$ ${credit - debt}`} text="Valor Consolidado" />
           </Content>
         </Row>
       </div>
@@ -21,4 +31,15 @@ class DashBoard extends Component {
   }
 }
 
-export default DashBoard;
+
+const mapStateToProps = (state) => {
+  return {
+    summary: state.dashboard.summary
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({getSummary}, dispatch);
+}
+export default connect(mapStateToProps,mapDispatchToProps)(DashBoard);
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {applyMiddleware,createStore} from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 
 import App from './components/app.jsx';
+import reducers from './reducers';
 
-render(<App/>, document.querySelector('#app'));
+const store = applyMiddleware(promise)(createStore)(reducers);
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  , document.querySelector('#app'));
