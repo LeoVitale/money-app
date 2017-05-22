3864e348173234f874ae0b9cc703b2bb54a1f97b
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-05-22 17:30:14 -0300

    Added Redux Dev Tools

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,10 @@ import promise from 'redux-promise';
 import App from './components/app.jsx';
 import reducers from './reducers';
 
-const store = applyMiddleware(promise)(createStore)(reducers);
+const DEV_TOOLS = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise)(createStore)(reducers, DEV_TOOLS);
+
+
 
 render(
   <Provider store={store}>
